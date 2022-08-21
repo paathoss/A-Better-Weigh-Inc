f26ac0f5ca4740bac0a7010c8ecebfdd69110f8c
patricio <santoropatricio1@gmail.com>
2022-08-21 02:28:36 -0300

    Final

---
+++ script/main.js
@@ -0,0 +1,28 @@
+function show() {
+  var toggle = document.getElementById('sidebar').classList.toggle('active');
+}
+
+
+var countDownDate = new Date("Aug 23, 2022 15:37:25").getTime();
+
+var x = setInterval(function () {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
